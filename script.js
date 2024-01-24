8071094c08ab69d31fb4d6ddf7b8746ada0e82a4
Beamemrrrer <156398691+Beamemrrrer@users.noreply.github.com>
2024-01-24 11:07:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function signup() {
+    var username = document.getElementById("signupUsername").value;
+    var password = document.getElementById("signupPassword").value;
+    // Perform signup logic (e.g., send data to server)
+    console.log("Sign Up - Username: " + username + ", Password: " + password);
+}
+
+function login() {
+    var username = document.getElementById("loginUsername").value;
+    var password = document.getElementById("loginPassword").value;
+    // Perform login logic (e.g., send data to server)
+    console.log("Login - Username: " + username + ", Password: " + password);
+}
